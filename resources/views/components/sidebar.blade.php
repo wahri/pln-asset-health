b9020457ea4fb93ffd8a0cdaeb1eae99724e140a
hadiid-studentcode <hadiidandri2000@gmail.com>
2024-09-23 15:13:11 +0700

    sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -27,7 +27,7 @@
                   <div class="menu-title">Asset Health Report</div>
               </a>
           </li>
-            <li class="{{ request()->routeIs('assetManagement.location.index') ? 'mm-active' : '' }}">
+            <li class="{{ request()->routeIs('assetManagement.*') ? 'mm-active' : '' }}">
               <a href="{{ route('assetManagement.location.index') }}" class="">
                   <div class="parent-icon"><i class="bx bx-command"></i></div>
                   <div class="menu-title">Asset Management</div>
