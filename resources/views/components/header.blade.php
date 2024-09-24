869a7a14c93464beee149ece1492c03bb71496a5
hadiid-studentcode <hadiidandri2000@gmail.com>
2024-09-24 21:45:00 +0700

    auth username pada dashboard

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -394,7 +394,7 @@
                       role="button" data-bs-toggle="dropdown" aria-expanded="false">
                       <img src="{{ asset('assets/images/avatars/avatar-2.png')}} "class="user-img" alt="user avatar" />
                       <div class="user-info ps-3">
-                          <p class="user-name mb-0">Wahyu Bahri</p>
+                          <p class="user-name mb-0">{{ Auth::user()->name }}</p>
                           <p class="designattion mb-0">IT Support</p>
                       </div>
                   </a>
